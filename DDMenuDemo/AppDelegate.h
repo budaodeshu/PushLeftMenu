c87c946b6491029d649a9789c05cabc0517c8eff
zheng <zhengliangshu@163.com>
2013-05-11 17:47:36 +0800

    Initial Commit

---
+++ DDMenuDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DDMenuDemo
+//
+//  Created by zheng on 13-5-11.
+//  Copyright (c) 2013年 zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
